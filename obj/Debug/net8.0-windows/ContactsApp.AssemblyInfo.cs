c59bcab33a32a073f4b1bb2a672c6533cdfbc8bb
Trenton Scott <InsaneGriot@Outlook.com>
2025-05-10 15:10:57 -0400

    NewContactWindow

--- obj/Debug/net8.0-windows/ContactsApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/ContactsApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f04744d178351615fddeffd6d595760b5fadb37b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
